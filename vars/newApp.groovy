08fafe598f6363addf77f6288f6ead3cc3072e8a
enwokocha <ezedahos1@yahoo.com>
2023-07-17 12:25:47 -0500

    vars newApp updated
    Signed-off-by: enwokocha <ezedahos1@yahoo.com>

--- vars/newApp.groovy
+++ vars/newApp.groovy
@@ -1,24 +1,24 @@
 def uber(String repoUrl){
-pipeline {
-    agent any
-    stages{
-        stage('1-build'){
-            steps{
-                sh 'free -g'
+    pipeline {
+        agent any
+        stages{
+            stage('1-build') {
+                steps {
+                    sh 'free -g'
+                }
             }
-        }
-        stage("Checkout Code") {
-            steps {
-                git branch: 'main',
-                        url: "${repoUrl}"
+            stage("Checkout Code") {
+                steps {
+                    git branch: 'main',
+                            url: "${repoUrl}"
+                }
             }
-        }
-        stage('closing'){
-            steps{
-                echo "new demo"
-                echo "complete"
+            stage('closing'){
+                steps{
+                    echo "new demo"
+                    echo "complete"
+                }
             }
         }
     }
-}
 }
