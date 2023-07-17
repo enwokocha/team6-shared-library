658719515c2087518983c134428cbc37a6f046e5
enwokocha <ezedahos1@yahoo.com>
2023-07-16 19:16:32 -0500

    var code modified
    Signed-off-by: enwokocha <ezedahos1@yahoo.com>

--- vars/etechApp.groovy
+++ vars/etechApp.groovy
@@ -1,24 +1,24 @@
 def call(string repoUrl){
-        pipeline {
-       agent any
-       stages {
-           stage("Tools initialization") {
-               steps {
-                   sh 'lscpu'
-                   sh 'java -version'
-               }
-           }
-           stage("Checkout Code") {
-               steps {
+pipeline {
+    agent any
+    stages {
+        stage("Tools initialization") {
+            steps {
+                sh 'lscpu'
+                sh 'java -version'
+            }
+        }
+        stage("Checkout Code") {
+            steps {
                    git branch: 'main',
                           url: "${repoUrl}"
-               }
-           }
-           stage("to-test-maven") {
-               steps {
+            }
+        }
+        stage("to-test-maven") {
+            steps {
                    sh 'df -h'
-               }
-           }
-       }
-   }
+            }
+        }
+    }
+}
 }
